5b29bb7e9ccb74e0d634bdda811c7e4d78b90eb6
Daniel Terry <daniel-terry@lambdastudents.com>
2021-05-07 13:12:09 -0400

    satrted laying out react components for main page

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Mern Shoppe</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
